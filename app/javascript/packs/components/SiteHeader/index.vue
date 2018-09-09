c1050972fcb3333c4140a230f7652b2b3b9f8194
albert fang <hr00090241@gmail.com>
2018-09-09 16:25:54 +0800

    Dashboard layout

--- app/javascript/packs/components/SiteHeader/index.vue
+++ app/javascript/packs/components/SiteHeader/index.vue
@@ -1,7 +1,8 @@
 <template>
   <el-header>
+    <a class="logo" href="/">Sunny</a>
     <el-menu :default-active="currentPath" mode="horizontal">
-      <el-menu-item index="1">Sunny</el-menu-item>
+
       <el-menu-item :index="costPath"><a :href="costPath">費用總表</a></el-menu-item>
     </el-menu>
   </el-header>
@@ -15,16 +16,23 @@ export default {
 
 
 <style lang="scss" scoped>
+.logo {
+  display: inline-block;
+  vertical-align: top;
+  height: 60px;
+  line-height: 60px;
+  color: white;
+  text-decoration: none;
+  padding-right: 20px;
+}
+
 .el-menu {
+  display: inline-block;
   background: transparent;
   color: white;
 
   .el-menu-item {
     color: white;
-
-    &:first-child{
-      padding-left: 0;
-    }
 
     &:not(.is-disabled):hover {
       background: rgba(#fff, 0.3);
